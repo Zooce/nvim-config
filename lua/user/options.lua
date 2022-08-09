38e59fce0fb58915e84628281dda47726c0671f1
Zooce <zooce@hey.com>
2022-08-09 00:02:31 -0700

    first try with lua config options

---
+++ lua/user/options.lua
@@ -0,0 +1,27 @@
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cmdheight = 2
+vim.opt.expandtab = true
+vim.opt.fileencoding = "utf-8"
+vim.opt.hidden = true
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.path:append "**"
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 4
+vim.opt.shiftwidth = 4
+vim.opt.shortmess:append "c"
+--vim.opt.showmode = false
+vim.opt.sidescrolloff = 4
+vim.opt.signcolumn = "yes"
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.softtabstop = 4
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 4
+vim.opt.updatetime = 300
+vim.opt.wildmenu = true
+vim.opt.wrap = false
+vim.opt.writebackup = false
