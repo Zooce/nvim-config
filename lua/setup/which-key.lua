c908ace4a975d0626cdc8ae06decaae7a0835b26
Zooce <zooce@hey.com>
2022-11-02 00:07:37 -0700

    use which-key for more telescope bindings

--- lua/setup/which-key.lua
+++ lua/setup/which-key.lua
@@ -3,12 +3,18 @@ local wk = require('which-key')
 wk.register({
     ['<Leader>f'] = { 
         name = '+find',
+        b = { '<Cmd>Telescope buffers<CR>', 'Find Buffer' },
         f = { '<Cmd>Telescope find_files<CR>', 'Find File' },
         g = { '<Cmd>Telescope live_grep<CR>', 'Live Grep' },
-        s = { '<Cmd>Telescope grep_string<CR>', 'Find String' },
-        w = { '<Cmd>lua require("telescope.builtin").grep_string({word_match="-w"})<CR>', 'Find Word' },
-        b = { '<Cmd>Telescope buffers<CR>', 'Find Buffer' },
         h = { '<Cmd>Telescope help_tags<CR>', 'Find Help' },
-    }
+        d = { '<Cmd>Telescope lsp_document_symbols<CR>', 'Find Document Symbol' },
+        s = { '<Cmd>Telescope lsp_workspace_symbols<CR>', 'Find Workspace Symbol' },
+        r = { '<Cmd>Telescope lsp_references<CR>', 'Find References' },
+        w = { '<Cmd>lua require("telescope.builtin").grep_string({word_match="-w"})<CR>', 'Find Word' },
+    },
+    ['<Leader>g'] = {
+        name = '+goto',
+        d = { '<Cmd>Telescope lsp_definitions<CR>', 'Goto Definition' },
+    },
 })
 
