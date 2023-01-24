35d6842516bc8c3112f8f7b120c74cf6678cee52
Zooce <zooce@hey.com>
2023-01-23 23:08:53 -0800

    fix/update keymaps

--- lua/helpers.lua
+++ lua/helpers.lua
@@ -1,10 +1,16 @@
 local M = {}
 
+M.keymap = function(modes, keys, func, desc)
+  vim.keymap.set(modes, keys, func, { noremap = true, silent = true, desc = desc })
+end
 M.nmap = function(keys, func, desc)
-  vim.keymap.set('n', keys, func, { noremap = true, silent = true, desc = desc })
+  M.keymap('n', keys, func, { noremap = true, silent = true, desc = desc })
 end
 M.vmap = function(keys, func, desc)
-  vim.keymap.set('v', keys, func, { noremap = true, silent = true, desc = desc })
+  M.keymap('v', keys, func, { noremap = true, silent = true, desc = desc })
+end
+M.xmap = function(keys, func, desc)
+  M.keymap('x', keys, func, { noremap = true, silent = true, desc = desc })
 end
 
 return M
