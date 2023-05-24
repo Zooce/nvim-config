01331f7a038148208b640afc08a877720c3b8b1f
Zooce <zooce@hey.com>
2023-05-23 23:44:13 -0700

    lua_ls issue is somewhere in v3.6.20

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,27 +24,27 @@ return {
         -- Lua LSP resources:
         -- * https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/lua_ls.lua
         -- * https://github.com/LuaLS/lua-language-server/wiki/Settings
-        -- lua_ls = {
-        --   Lua = {
-        --     runtime = {
-        --       -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        --       version = 'LuaJIT',
-        --     },
-        --     diagnostics = {
-        --       -- Get the language server to recognize the `vim` global
-        --       globals = { 'vim' },
-        --     },
-        --     workspace = {
-        --       -- Make the server aware of Neovim runtime files
-        --       library = vim.api.nvim_get_runtime_file("", true),
-        --       checkThirdParty = false,
-        --     },
-        --     -- Do not send telemetry data containing a randomized but unique identifier
-        --     telemetry = {
-        --       enable = false,
-        --     },
-        --   },
-        -- },
+        lua_ls = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+              version = 'LuaJIT',
+            },
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { 'vim' },
+            },
+            workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
         svelte = {},
       }
 
