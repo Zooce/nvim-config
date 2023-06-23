d4e5920d38ab086fccdff4e25480531bdec2030e
Zooce <zooce@hey.com>
2023-06-22 22:55:23 -0700

    fix desc bug in keymap helpers

--- lua/helpers.lua
+++ lua/helpers.lua
@@ -4,13 +4,13 @@ M.keymap = function(modes, keys, func, desc)
   vim.keymap.set(modes, keys, func, { noremap = true, silent = true, desc = desc })
 end
 M.nmap = function(keys, func, desc)
-  M.keymap('n', keys, func, { noremap = true, silent = true, desc = desc })
+  M.keymap('n', keys, func, desc)
 end
 M.vmap = function(keys, func, desc)
-  M.keymap('v', keys, func, { noremap = true, silent = true, desc = desc })
+  M.keymap('v', keys, func, desc)
 end
 M.xmap = function(keys, func, desc)
-  M.keymap('x', keys, func, { noremap = true, silent = true, desc = desc })
+  M.keymap('x', keys, func, desc)
 end
 
 return M
