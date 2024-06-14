275131936aac898b81d896b7c42e61b548156da6
Zooce <zooce@hey.com>
2024-06-14 11:52:44 -0700

    fix(lsp): default lsp setup function

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -35,6 +35,9 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
       local handlers = {
+        function(server)
+          require('lspconfig')[server].setup {}
+        end,
         ['rust_analyzer'] = function()
           require('lspconfig').rust_analyzer.setup {
             capabilities = capabilities,
