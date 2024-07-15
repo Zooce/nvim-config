21c820b1686eb40f1be0812b17956e47210e1cf5
Zooce <zooce@hey.com>
2024-07-15 08:56:55 -0700

    fix(which-key): update config for v3

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -35,7 +35,7 @@ return {
       vim.o.timeout = true
       vim.o.timeoutlen = 300
       require('which-key').setup{
-        window = { border = 'single' },
+        win = { border = 'single' },
       }
     end,
   },
