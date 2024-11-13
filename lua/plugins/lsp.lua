04082c6a53fd9f6293366d56a89fda1db0dbee85
Zooce <zooce@hey.com>
2024-11-13 05:48:16 -0800

    fix(lsp): replace deprecated buf config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,6 @@
 -- set up single borders for lsp
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-  vim.lsp.handlers.hover, {
-    border = 'single',
-  }
-)
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-  vim.lsp.handlers.signature_help, {
-    border = 'single',
-  }
-)
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.buf.hover({ border = 'single' })
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.buf.signature_help({ border = 'single' })
 vim.diagnostic.config{
   float = { border = 'single' },
 }
