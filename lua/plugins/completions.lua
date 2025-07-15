ad5805fe2f23e572d4c7504a44074ec575e457f0
Zooce <zooce@hey.com>
2025-07-15 10:36:49 -0700

    feat(cmp): turn completions off

---
+++ lua/plugins/completions.lua
@@ -0,0 +1,11 @@
+-- I don't like autocomplete...it's too fucking noisy
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    enabled = false,
+  },
+  {
+    "saghen/blink.cmp",
+    enabled = false,
+  },
+}
