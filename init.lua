a366f1fc96779e328eb56551b803f72d9fa89483
Zooce <zooce@hey.com>
2025-08-08 11:35:21 -0700

    chore(autocmd): update yank highlight group

--- init.lua
+++ init.lua
@@ -269,12 +269,12 @@ vim.keymap.set("n", "<Leader>i", ":Inspect<CR>")
 -- ================================================================================
 
 -- highlight yank
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+local yank_highlight = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
   end,
-  group = highlight_group,
+  group = yank_highlight,
   pattern = '*',
 })
 
