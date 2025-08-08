b6ba9512e7f8074388295605b98a747fef0f71b9
Zooce <zooce@hey.com>
2025-08-08 11:32:40 -0700

    fix(lsp): forgot filetypes in ruby_lsp

--- lsp/ruby_lsp.lua
+++ lsp/ruby_lsp.lua
@@ -1,6 +1,7 @@
 return {
     name = "ruby_lsp",
     cmd = { "ruby-lsp" },
+    filetypes = { "ruby", "eruby" },
     root_markers = { "Gemfile", ".git" },
     before_init = function(_, _)
         vim.notify_once("Starting Ruby LSP...")
