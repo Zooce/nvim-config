eda1661e66ba7ea370781d25c010b11b227c224f
Zooce <zooce@hey.com>
2025-08-31 18:30:17 -0700

    feat(lsp): add laravel lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -35,4 +35,23 @@ return {
     ft = 'lua',
     config = true,
   },
+  -- Laravel (wip)
+  {
+    "adalessa/laravel.nvim",
+    dependencies = {
+      "tpope/vim-dotenv",
+      "nvim-telescope/telescope.nvim",
+      "MunifTanjim/nui.nvim",
+      "kevinhwang91/promise-async",
+    },
+    cmd = { "Laravel" },
+    keys = {
+      -- { "<leader>la", ":Laravel artisan<cr>" },
+      -- { "<leader>lr", ":Laravel routes<cr>" },
+      -- { "<leader>lm", ":Laravel related<cr>" },
+    },
+    event = { "VeryLazy" },
+    opts = {},
+    config = true,
+  }
 }
